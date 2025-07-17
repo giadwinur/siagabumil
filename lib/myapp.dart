fec04ffd32815aa87208caeb3d9551f317a672bc
giadwinur <giadwi574@gmail.com>
2025-07-17 10:39:08 +0700

    awal

---
+++ lib/myapp.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:siagabumil/home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+      home: const Home(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
