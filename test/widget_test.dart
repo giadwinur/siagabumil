5cd70ffd2ee606d99c8f2c6687e8a6f224544764
giadwinur <giadwi574@gmail.com>
2025-07-17 10:43:25 +0700

    coba

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+// ignore: unused_import
 import 'package:siagabumil/main.dart';
+import 'package:siagabumil/myapp.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
